8ffc15e35a6adcbe066d628e70c58ed99ac4af0e
Saraswathi8709 <saraswathiagrahara26@gmail.com>
2026-03-11 13:38:54 +0530

    initial commit

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ def home():
     return "CI/CD Pipeline is running successfully!"
 if __name__ == "__main__":
     app.run()
+
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
