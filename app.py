89d4b931445a87d0e5901fa4800a0d82c0363587
Saraswathi8709 <saraswathiagrahara26@gmail.com>
2026-03-11 13:49:45 +0530

    Improved UI for CI/CD project

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline is running successfully!"
+    return render_template("index.html")
+
 if __name__ == "__main__":
-    app.run()
-
-from app import app
-
-def test_home():
-    client = app.test_client()
-    response = client.get("/")
-    assert response.status_code == 200
+    app.run(debug=True)
